a9d7263be8f989101fdcbc4c1a4c1cbda4b7294c
Anna <annak@iprosis.com>
2019-11-19 14:12:36 +0200

    try

--- webcomponent.js
+++ webcomponent.js
@@ -1,9 +1,77 @@
 (function()  {
-let tmpl = document.createElement('template');
-tmpl.innerHTML = `
-<input type="text" class="form-control" id="searchMovie" value="">
-
-`;
+	let tmpl = document.createElement('template');
+	tmpl.innerHTML = `
+	<script type="text/javascript">
+	window.onload = function(){ 
+		//Get submit button
+		var submitbutton = document.getElementById("tfq");
+		//Add listener to submit button
+		if(submitbutton.addEventListener){
+			submitbutton.addEventListener("click", function() {
+				if (submitbutton.value == 'Search our website'){//Customize this text string to whatever you want
+					submitbutton.value = '';
+				}
+			});
+		}
+	}
+	</script>
+	  <style type="text/css">
+		#tfheader{
+			background-color:#c3dfef;
+		}
+		#tfnewsearch{
+			float:right;
+			padding:20px;
+		}
+		.tftextinput2{
+			margin: 0;
+			padding: 5px 15px;
+			font-family: Arial, Helvetica, sans-serif;
+			font-size:14px;
+			color:#666;
+			border:1px solid #0076a3; border-right:0px;
+			border-top-left-radius: 5px 5px;
+			border-bottom-left-radius: 5px 5px;
+		}
+		.tfbutton2 {
+			margin: 0;
+			padding: 5px 7px;
+			font-family: Arial, Helvetica, sans-serif;
+			font-size:14px;
+			font-weight:bold;
+			outline: none;
+			cursor: pointer;
+			text-align: center;
+			text-decoration: none;
+			color: #ffffff;
+			border: solid 1px #0076a3; border-right:0px;
+			background: #0095cd;
+			background: -webkit-gradient(linear, left top, left bottom, from(#00adee), to(#0078a5));
+			background: -moz-linear-gradient(top,  #00adee,  #0078a5);
+			border-top-right-radius: 5px 5px;
+			border-bottom-right-radius: 5px 5px;
+		}
+		.tfbutton2:hover {
+			text-decoration: none;
+			background: #007ead;
+			background: -webkit-gradient(linear, left top, left bottom, from(#0095cc), to(#00678e));
+			background: -moz-linear-gradient(top,  #0095cc,  #00678e);
+		}
+		/* Fixes submit button height problem in Firefox */
+		.tfbutton2::-moz-focus-inner {
+		  border: 0;
+		}
+		.tfclear{
+			clear:both;
+		}
+	</style>
+	  <div id="tfheader">
+			<form id="tfnewsearch" method="get" action="http://www.google.com">
+					<input type="text" id="tfq" class="tftextinput2" name="q" size="21" maxlength="120" value="Search our website"><input type="submit" value=">" class="tfbutton2">
+			</form>
+			<div class="tfclear"></div>
+		</div>
+	`;
 
 class ISearch extends HTMLElement {
 
@@ -12,20 +80,52 @@ class ISearch extends HTMLElement {
 		this._shadowRoot = this.attachShadow({mode: 'open'});
 		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 		this.sVal = 0;
+		this.stext ="";
+		this.sena=true;
+		this.splaceholder="";
+		this.sHeader="HEAD";
+
+		
+	// TRY CODE IN CONSTRUCTOR INSTED OF INNER HTML	
+	window.onload = function(){ 
+		//Get submit button
+		var submitbutton = document.getElementById("tfq");
+		//Add listener to submit button
+		if(submitbutton.addEventListener){
+			submitbutton.addEventListener("onClick", function() {
+				if (submitbutton.value == 'Search our website'){//Customize this text string to whatever you want
+					submitbutton.value = '';
+				}
+			});
+		}
+	
+	}
 
 		
 	};
 
-	
-	
-
-    get val() {
+	 get val() {
     	return this.sVal;
     }
     set val(value) {
 		this.sVal =  value;
 		
 	};
+	get txt(){
+		return this.stxt;
+	}
+	set senabled(senaRes){
+		this.sena=senaRes;
+	}
+	get isEnabled(){
+		return this.sena;
+	}
+	get placeholder(){
+		return this.splaceholder;
+	}
+	set placeholder(splaceholder){
+		this.splaceholder=splaceholder;
+	}
 
   }
 	  /* Define web component - input: tag and class */
