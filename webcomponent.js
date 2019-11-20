de2573058b409b604edac729f8e70631a93eeea6
Anna <annak@iprosis.com>
2019-11-20 10:59:18 +0200

    1

--- webcomponent.js
+++ webcomponent.js
@@ -65,12 +65,6 @@
 			clear:both;
 		}
 	</style>
-	  <div id="tfheader">
-			<form id="tfnewsearch" method="get" action="http://www.google.com">
-					<input type="text" id="tfq" class="tftextinput2" name="q" size="21" maxlength="120" value="Search our website"><input type="submit" value=">" class="tfbutton2">
-			</form>
-			<div class="tfclear"></div>
-		</div>
 	`;
 
 class ISearch extends HTMLElement {
@@ -80,283 +74,284 @@ class ISearch extends HTMLElement {
 		this._shadowRoot = this.attachShadow({mode: 'open'});
 		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 		var dataResultSet = null;
-			var _isEnabled = null;
-			var placeHolder = null;
-			var _isSearchButton = null;
-			var isSuggestions = null;
-			var maxLength = null;
-			var selectedDimension = null;
+		var isEnabled = null;
+		var placeHolder = null;
+		var isSearchButton = null;
+		var isSuggestions = null;
+		var maxLength = null;
+		var selectedDimension = null;
+		
+		var displayKey = null;
+		var selectedValue = null;
+		var selectedText = null;
+		var reload = false;
+		var that = this;
+		var Data = null;
+		this.oSearchField = null;
+		
+		this.init = function() {
 			
-			var displayKey = null;
-			var selectedValue = null;
-			var selectedText = null;
-			var reload = false;
-			var that = this;
-			var Data = null;
-			this.oSearchField = null;
+			if( window.sap && sap.zen && sap.zen.designmode)
+				{
+					var x = "dd";
+				}
 			
-			this.init = function() {
+			
+			if (this._alive){
+				return;
+			} else {
+			
+				currentDiv = "DIV_" + this.$().attr('id');
+				var currentSf = "SF_" + this.$().attr('id');
 				
-				if( window.sap && sap.zen && sap.zen.designmode)
-					{
-						var x = "dd";
+				// Create Search Field control and load data
+				that.oSearchField = new sap.m.SearchField(currentSf, {
+					enableSuggestions: true,
+					search: function(oEvent) {
+						var text = "";
+						var key = "";
+						var isFire = true;
+						if (isSuggestions === false)
+							{
+								text = oEvent.getParameter("query");
+								key = text;
+							}else{
+								var item = oEvent.getParameter("suggestionItem");
+								if(item)
+									{
+										text = item.getText();
+										key = item.getKey();
+									}else if (oEvent.getParameter("query") === selectedText)
+										{
+										 	isFire = false;
+										}
+							}
+				//		oEvent.getParameter("query");
+							if (isFire)
+								{
+									selectedValue = key;
+									selectedText = text;
+						            that.firePropertiesChanged(["SelectedValue"]);
+						            that.firePropertiesChanged(["SelectedText"]);
+						            that.fireEvent("onSearch");  
+								}
+					},
+
+					suggest: function(oEvent) {
+							var value =oEvent.getParameter("suggestValue");
+							var filters = [];
+							if(value !== "")
+								{
+								 filters = that.getFilters(value);		
+								}else{
+									filters = that.getFilters("999999iprosis");		
+								}
+							that.oSearchField.getBinding("suggestionItems").filter(filters);
+							that.oSearchField.suggest();
 					}
-				
-				
-				if (this._alive){
+				});
+
+			this.$().html('<div id="' + currentDiv + '"> ');
+			that.oSearchField.placeAt(currentDiv);
+			this._alive = true;
+			}
+		};
+		
+		this.afterUpdate = function() {
+			that.oSearchField.setEnabled(isEnabled);
+			that.oSearchField.setPlaceholder(placeHolder);
+			that.oSearchField.setEnableSuggestions(isSuggestions);
+			that.oSearchField.setMaxLength(maxLength);
+			that.oSearchField.setShowSearchButton(isSearchButton);
+
+			if (dataResultSet){
+				if (reload){	
 					return;
 				} else {
-				
-					currentDiv = "DIV_" + this.$().attr('id');
-					var currentSf = "SF_" + this.$().attr('id');
-					
-					// Create Search Field control and load data
-					that.oSearchField = new sap.m.SearchField(currentSf, {
-						enableSuggestions: true,
-						search: function(oEvent) {
-							var text = "";
-							var key = "";
-							var isFire = true;
-							if (isSuggestions === false)
-								{
-									text = oEvent.getParameter("query");
-									key = text;
-								}else{
-									var item = oEvent.getParameter("suggestionItem");
-									if(item)
-										{
-											text = item.getText();
-											key = item.getKey();
-										}else if (oEvent.getParameter("query") === selectedText)
-											{
-												 isFire = false;
-											}
-								}
-					//		oEvent.getParameter("query");
-								if (isFire)
-									{
-										selectedValue = key;
-										selectedText = text;
-										that.firePropertiesChanged(["SelectedValue"]);
-										that.firePropertiesChanged(["SelectedText"]);
-										that.fireEvent("onSearch");  
-									}
-						},
-	
-						suggest: function(oEvent) {
-								var value =oEvent.getParameter("suggestValue");
-								var filters = [];
-								if(value !== "")
-									{
-									 filters = that.getFilters(value);		
-									}else{
-										filters = that.getFilters("999999iprosis");		
-									}
-								that.oSearchField.getBinding("suggestionItems").filter(filters);
-								that.oSearchField.suggest();
-						}
-					});
-	
-				this.$().html('<div id="' + currentDiv + '"> ');
-				that.oSearchField.placeAt(currentDiv);
-				this._alive = true;
+					this.insertData();
+					reload = true;
 				}
-			};
-			
-			this.afterUpdate = function() {
-				that.oSearchField.setEnabled(_isEnabled);
-				that.oSearchField.setPlaceholder(placeHolder);
-				that.oSearchField.setEnableSuggestions(isSuggestions);
-				that.oSearchField.setMaxLength(maxLength);
-				that.oSearchField.setShowSearchButton(_isSearchButton);
-	
-				if (dataResultSet){
-					if (reload){	
-						return;
-					} else {
-						this.insertData();
-						reload = true;
-					}
-				}
-				
-			};
-			this.getFilters = function(value){
-				var filters = [];
-				oFilterText = new sap.ui.model.Filter("text", function(sText) {
-					return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-				});
-				oFilterDesc =  new sap.ui.model.Filter("key", function(sDes) {
-						 return (sDes || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-					 });
-				
-				if (displayKey === true)
-					{
-						filters = [new sap.ui.model.Filter([oFilterText,oFilterDesc], false)];
-					}else{
-						filters = [	new sap.ui.model.Filter([oFilterText], false)];
-					}
-				
-				return filters
-			};
-			this.getTemplate = function(){
-				if (displayKey === true)
-					{
-						 oTemplate = new sap.m.SuggestionItem({
-							text: "{text}",
-							key: "{key}",
-							description:"{key}"
-						 });
-					}else{
-						oTemplate = new sap.m.SuggestionItem({
-						text: "{text}",
-						key: "{key}"
-					});
-				}
-				return oTemplate;
-			};
-			
-			
-			this.getData = function(result){
-				var data = [];
-				var x = result;
-				var obj = {};
-				result.forEach(function(element) {
-					obj = {};
-					obj.text = element.text;
-					obj.key = element.key;
-					data.push(obj);
-				});
-			
-				var oModel = new sap.ui.model.json.JSONModel(data);
-				var oTemplate = this.getTemplate();
-				that.oSearchField.setModel(oModel);
-				that.oSearchField.bindAggregation("suggestionItems", "/", oTemplate);	
-				
 			}
 			
-			this.insertData = function() {
-	
-				if (selectedDimension)
-					{
-					var dim = selectedDimension.toUpperCase();
-						that.callZTLFunction("getMembers", that.getData, dim);
-					}
-						
-	//			data = dataResultSet.dimensions[0].members;
-	//			oModel.setSizeLimit(maxItems);			
-			};
+		};
+		this.getFilters = function(value){
+			var filters = [];
+			oFilterText = new sap.ui.model.Filter("text", function(sText) {
+				return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
+			});
+			oFilterDesc =  new sap.ui.model.Filter("key", function(sDes) {
+				 	return (sDes || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
+				 });
 			
-			this.DataResultSet = function(value) {
-				if(value===undefined) {
-					return dataResultSet;
-				} else {
-					//Clear Auto
-					if (reload) {
-						that.oSearchField.removeAllSuggestionItems();
-						that.oSearchField.setValue("");
-						reload = false;
-					}
-					dataResultSet = value;
-					return this;
-				};
-			};	
+			if (displayKey === true)
+				{
+					filters = [new sap.ui.model.Filter([oFilterText,oFilterDesc], false)];
+				}else{
+					filters = [	new sap.ui.model.Filter([oFilterText], false)];
+				}
 			
-			this.DisplayKey = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return displayKey;
-				} else {
-					Reload = false;
-					displayKey = value;
-					return this;
-				};
-			};
-	
-			this.IsEnabled = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return _isEnabled;
-				} else {
-					Reload = false;
-					_isEnabled = value;
-					return this;
-				};
-			};
-			
-			this.PlaceHolder = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return placeHolder;
-				} else {
-					Reload = false;
-					placeHolder = value;
-					return this;
-				};
-			};
-			
-			this.IsSearchButton = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return _isSearchButton;
-				} else {
-					Reload = false;
-					_isSearchButton = value;
-					return this;
-				};
-			};
-			this.MaxLength = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return maxLength;
-				} else {
-					Reload = false;
-					maxLength = value;
-					return this;
-				};
-			};
-			this.SelectedDimension = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return selectedDimension;
-				} else {
-					Reload = false;
-					selectedDimension = value;
-					return this;
-				};
-			};
-			
-			
-			this.IsSuggestions = function(value) {
-				if(value===undefined) {
-					Reload = false;
-					return isSuggestions;
-				} else {
-					Reload = false;
-					isSuggestions = value;
-					return this;
-				};
-			};
+			return filters
+		};
+		this.getTemplate = function(){
+			if (displayKey === true)
+				{
+					 oTemplate = new sap.m.SuggestionItem({
+						text: "{text}",
+						key: "{key}",
+						description:"{key}"
+					 });
+				}else{
+					oTemplate = new sap.m.SuggestionItem({
+					text: "{text}",
+					key: "{key}"
+				});
+			}
+			return oTemplate;
+		};
 		
-			this.SelectedValue = function(value) {
-				if(value===undefined) {
-					return selectedValue;
-				} else {
-					selectedValue = value;
-					return this;
-				};
+		
+		this.getData = function(result){
+			var data = [];
+			var x = result;
+			var obj = {};
+			result.forEach(function(element) {
+				obj = {};
+				obj.text = element.text;
+				obj.key = element.key;
+				data.push(obj);
+			});
+		
+			var oModel = new sap.ui.model.json.JSONModel(data);
+			var oTemplate = this.getTemplate();
+			that.oSearchField.setModel(oModel);
+			that.oSearchField.bindAggregation("suggestionItems", "/", oTemplate);	
+			
+		}
+		
+		this.insertData = function() {
+
+			if (selectedDimension)
+				{
+				var dim = selectedDimension.toUpperCase();
+					that.callZTLFunction("getMembers", that.getData, dim);
+				}
+					
+//			data = dataResultSet.dimensions[0].members;
+//			oModel.setSizeLimit(maxItems);			
+		};
+		
+		this.DataResultSet = function(value) {
+			if(value===undefined) {
+				return dataResultSet;
+			} else {
+				//Clear Auto
+				if (reload) {
+					that.oSearchField.removeAllSuggestionItems();
+					that.oSearchField.setValue("");
+					reload = false;
+				}
+				dataResultSet = value;
+				return this;
 			};
-			
-			this.SelectedText = function(value) {
-				if(value===undefined) {
-					return selectedText;
-				} else {
-					selectedText = value;
-					return this;
-				};
-			};	
-			
 		};	
-	}
+		
+		this.DisplayKey = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return displayKey;
+			} else {
+				Reload = false;
+				displayKey = value;
+				return this;
+			};
+		};
+
+		this.IsEnabled = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return isEnabled;
+			} else {
+				Reload = false;
+				isEnabled = value;
+				return this;
+			};
+		};
+		
+		this.PlaceHolder = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return placeHolder;
+			} else {
+				Reload = false;
+				placeHolder = value;
+				return this;
+			};
+		};
+		
+		this.IsSearchButton = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return isSearchButton;
+			} else {
+				Reload = false;
+				isSearchButton = value;
+				return this;
+			};
+		};
+		this.MaxLength = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return maxLength;
+			} else {
+				Reload = false;
+				maxLength = value;
+				return this;
+			};
+		};
+		this.SelectedDimension = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return selectedDimension;
+			} else {
+				Reload = false;
+				selectedDimension = value;
+				return this;
+			};
+		};
+		
+		
+		this.IsSuggestions = function(value) {
+			if(value===undefined) {
+				Reload = false;
+				return isSuggestions;
+			} else {
+				Reload = false;
+				isSuggestions = value;
+				return this;
+			};
+		};
+	
+		this.SelectedValue = function(value) {
+			if(value===undefined) {
+				return selectedValue;
+			} else {
+				selectedValue = value;
+				return this;
+			};
+		};
+		
+		this.SelectedText = function(value) {
+			if(value===undefined) {
+				return selectedText;
+			} else {
+				selectedText = value;
+				return this;
+			};
+		};	
+		
+	});	
+});
+
 
 	// TRY CODE IN CONSTRUCTOR INSTED OF INNER HTML	
 	window.onload = function(){ 
@@ -365,7 +360,7 @@ class ISearch extends HTMLElement {
 		//Add listener to submit button
 		if(submitbutton.addEventListener){
 			submitbutton.addEventListener("onClick", function() {
-				if (submitbutton.value == 'Search our website'){//Customize this text string to whatever you want
+				if (submitbutton.value == 'Search'){//Customize this text string to whatever you want
 					submitbutton.value = '';
 				}
 			});
