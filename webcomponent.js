4c4728bce6205a821d0695565836c8a7b00fc555
Anna <annak@iprosis.com>
2019-11-20 17:36:34 +0200

    try

--- webcomponent.js
+++ webcomponent.js
@@ -1,58 +1,30 @@
 (function()  {
 	let tmpl = document.createElement('template');
 	tmpl.innerHTML = `
+	<input type="text" name="inputbox" id="inputbox">
 	<style type="text/css">
-	#tfnewsearch{
-		float:right;
-		padding:20px;
+	input {
+		width: 200px;
 	}
-	.tftextinput2{
+	
+	input, .drop {
 		margin: 0;
-		padding: 5px 15px;
-		font-family: Arial, Helvetica, sans-serif;
-		font-size:14px;
-		color:#666;
-		border:1px solid #0076a3; border-right:0px;
-		border-top-left-radius: 5px 5px;
-		border-bottom-left-radius: 5px 5px;
+		padding: 0;
+		padding: 6px;
+		font-size: 16px;
+		font-family: "helvetica neue", helvetica, sans-serif;
 	}
-	.tfbutton2 {
-		margin: 0;
-		padding: 5px 7px;
-		font-family: Arial, Helvetica, sans-serif;
-		font-size:14px;
-		font-weight:bold;
-		outline: none;
-		cursor: pointer;
-		text-align: center;
-		text-decoration: none;
-		color: #ffffff;
-		border: solid 1px #0076a3; border-right:0px;
-		background: #0095cd;
-		background: -webkit-gradient(linear, left top, left bottom, from(#00adee), to(#0078a5));
-		background: -moz-linear-gradient(top,  #00adee,  #0078a5);
-		border-top-right-radius: 5px 5px;
-		border-bottom-right-radius: 5px 5px;
+	
+	.drop {
+		display: none;
+		list-style-type: none;
+		list-style-position: inside;
+		border-style: none groove groove groove;
+		border-color: silver;
+		border-width:5px;
+		width: 195px;
+		background-color: lightgray;
 	}
-	.tfbutton2:hover {
-		text-decoration: none;
-		background: #007ead;
-		background: -webkit-gradient(linear, left top, left bottom, from(#0095cc), to(#00678e));
-		background: -moz-linear-gradient(top,  #0095cc,  #00678e);
-	}
-	/* Fixes submit button height problem in Firefox */
-	.tfbutton2::-moz-focus-inner {
-	  border: 0;
-	}
-	.tfclear{
-		clear:both;
-	}
-	</style>
-	  <div id="tfheader">
-			<form id="tfnewsearch">
-					<input type="text" id="tfq" class="tftextinput2" name="q" size="21" maxlength="120" value=""><input type="submit" value=">" class="tfbutton2">
-			</form>
-			<div class="tfclear"></div>
 	</div>
 	`;
 
@@ -77,268 +49,62 @@ class ISearch extends HTMLElement {
 		var that = this;
 		var Data = null;
 		this.oSearchField = null;
-	
+		var list = [ 
+			{name: 'Afghanistan', code: 'AF'}, 
+			{name: 'Åland Islands', code: 'AX'}, 
+			{name: 'Albania', code: 'AL'}, 
+			{name: 'Algeria', code: 'DZ'}, 
+			{name: 'American Samoa', code: 'AS'}, 
+			{name: 'AndorrA', code: 'AD'}, 
+			{name: 'Angola', code: 'AO'}, 
+			{name: 'Anguilla', code: 'AI'}, 
+			{name: 'Antarctica', code: 'AQ'}
+		]; 
 		
-		this.init = function() {
-			
-			if( window.sap && sap.zen && sap.zen.designmode)
-				{
-					var x = "dd";
-				}
-			
-			
-			if (this._alive){
-				return;
-			} else {
-			
-				currentDiv = "DIV_" + this.$().attr('id');
-				var currentSf = "SF_" + this.$().attr('id');
-				
-				// Create Search Field control and load data
-				that.oSearchField = new sap.m.SearchField(currentSf, {
-					enableSuggestions: true,
-					search: function(oEvent) {
-						var text = "";
-						var key = "";
-						var isFire = true;
-						if (isSuggestions === false)
-							{
-								text = oEvent.getParameter("query");
-								key = text;
-							}else{
-								var item = oEvent.getParameter("suggestionItem");
-								if(item)
-									{
-										text = item.getText();
-										key = item.getKey();
-									}else if (oEvent.getParameter("query") === selectedText)
-										{
-										 	isFire = false;
-										}
-							}
-				//		oEvent.getParameter("query");
-							if (isFire)
-								{
-									selectedValue = key;
-									selectedText = text;
-						            that.firePropertiesChanged(["SelectedValue"]);
-						            that.firePropertiesChanged(["SelectedText"]);
-						            that.fireEvent("onSearch");  
-								}
-					},
-
-					suggest: function(oEvent) {
-							var value =oEvent.getParameter("suggestValue");
-							var filters = [];
-							if(value !== "")
-								{
-								 filters = that.getFilters(value);		
-								}else{
-									filters = that.getFilters("999999iprosis");		
-								}
-							that.oSearchField.getBinding("suggestionItems").filter(filters);
-							that.oSearchField.suggest();
+		function Autocomplete(selector) {
+		
+			this.list = list;
+		
+			this.box = document.getElementById(selector);
+		
+			this.dropdown = null;
+		
+			this.init = function() {
+				this.box.addEventListener('keyup', this.boxKeyUp);
+				this.dropdown = document.createElement('ul');
+				this.dropdown.setAttribute('class','drop');
+				document.body.appendChild(this.dropdown);
+				this.updateList();
+			};
+		
+			this.updateList = function() {
+				this.dropdown.innerHTML = '';
+				var value = this.box.value;
+				for(var i=0; i < this.list.length; i++) {
+					var name = list[i].name.toLowerCase();
+					var idx = name.indexOf(value);
+					if(idx===0) {
+						var item = document.createElement('li');
+						item.appendChild(document.createTextNode(list[i].name));
+						this.dropdown.appendChild(item);    
 					}
-				});
-
-			this.$().html('<div id="' + currentDiv + '"> ');
-			that.oSearchField.placeAt(currentDiv);
-			this._alive = true;
-			}
-		};
+				}     
+			};
 		
-		this.afterUpdate = function() {
-			that.oSearchField.setEnabled(isEnabled);
-			that.oSearchField.setPlaceholder(placeHolder);
-			that.oSearchField.setEnableSuggestions(isSuggestions);
-			that.oSearchField.setMaxLength(maxLength);
-			that.oSearchField.setShowSearchButton(isSearchButton);
-
-			if (dataResultSet){
-				if (reload){	
-					return;
-				} else {
-					this.insertData();
-					reload = true;
+			this.boxKeyUp = function() {
+				if(this.box.value===''){
+					this.dropdown.style.display = 'none';
+					return true;
 				}
-			}
-			
-		};
-		this.getFilters = function(value){
-			var filters = [];
-			oFilterText = new sap.ui.model.Filter("text", function(sText) {
-				return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-			});
-			oFilterDesc =  new sap.ui.model.Filter("key", function(sDes) {
-				 	return (sDes || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-				 });
-			
-			if (displayKey === true)
-				{
-					filters = [new sap.ui.model.Filter([oFilterText,oFilterDesc], false)];
-				}else{
-					filters = [	new sap.ui.model.Filter([oFilterText], false)];
-				}
-			
-			return filters
-		};
-		this.getTemplate = function(){
-			if (displayKey === true)
-				{
-					 oTemplate = new sap.m.SuggestionItem({
-						text: "{text}",
-						key: "{key}",
-						description:"{key}"
-					 });
-				}else{
-					oTemplate = new sap.m.SuggestionItem({
-					text: "{text}",
-					key: "{key}"
-				});
-			}
-			return oTemplate;
-		};
+				this.updateList();
+				this.dropdown.style.display = 'block';
+			}.bind(this);
 		
+			this.init();
 		
-		this.getData = function(result){
-			var data = [];
-			var x = result;
-			var obj = {};
-			result.forEach(function(element) {
-				obj = {};
-				obj.text = element.text;
-				obj.key = element.key;
-				data.push(obj);
-			});
-		
-			var oModel = new sap.ui.model.json.JSONModel(data);
-			var oTemplate = this.getTemplate();
-			that.oSearchField.setModel(oModel);
-			that.oSearchField.bindAggregation("suggestionItems", "/", oTemplate);	
-			
+			return this;
 		}
-		
-		this.insertData = function() {
-
-			if (selectedDimension)
-				{
-				var dim = selectedDimension.toUpperCase();
-					that.callZTLFunction("getMembers", that.getData, dim);
-				}
-					
-//			data = dataResultSet.dimensions[0].members;
-//			oModel.setSizeLimit(maxItems);			
-		};
-		
-		this.DataResultSet = function(value) {
-			if(value===undefined) {
-				return dataResultSet;
-			} else {
-				//Clear Auto
-				if (reload) {
-					that.oSearchField.removeAllSuggestionItems();
-					that.oSearchField.setValue("");
-					reload = false;
-				}
-				dataResultSet = value;
-				return this;
-			};
-		};	
-		
-		this.DisplayKey = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return displayKey;
-			} else {
-				Reload = false;
-				displayKey = value;
-				return this;
-			};
-		};
-
-		this.IsEnabled = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return isEnabled;
-			} else {
-				Reload = false;
-				isEnabled = value;
-				return this;
-			};
-		};
-		
-		this.PlaceHolder = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return placeHolder;
-			} else {
-				Reload = false;
-				placeHolder = value;
-				return this;
-			};
-		};
-		
-		this.IsSearchButton = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return isSearchButton;
-			} else {
-				Reload = false;
-				isSearchButton = value;
-				return this;
-			};
-		};
-		this.MaxLength = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return maxLength;
-			} else {
-				Reload = false;
-				maxLength = value;
-				return this;
-			};
-		};
-		this.SelectedDimension = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return selectedDimension;
-			} else {
-				Reload = false;
-				selectedDimension = value;
-				return this;
-			};
-		};
-		
-		
-		this.IsSuggestions = function(value) {
-			if(value===undefined) {
-				Reload = false;
-				return isSuggestions;
-			} else {
-				Reload = false;
-				isSuggestions = value;
-				return this;
-			};
-		};
-	
-		this.SelectedValue = function(value) {
-			if(value===undefined) {
-				return selectedValue;
-			} else {
-				selectedValue = value;
-				return this;
-			};
-		};
-		
-		this.SelectedText = function(value) {
-			if(value===undefined) {
-				return selectedText;
-			} else {
-				selectedText = value;
-				return this;
-			};
-		};	
-		
+		var ac = new Autocomplete('inputbox');
 	}};
 	  /* Define web component - input: tag and class */
 	  customElements.define('com-iprosis-sample-search', ISearch);
