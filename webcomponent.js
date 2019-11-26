8f2844614c4cfaaaf22a2c8b9bf354e7931018c7
Anna <annak@iprosis.com>
2019-11-26 10:24:33 +0200

    1

--- webcomponent.js
+++ webcomponent.js
@@ -1,38 +1,6 @@
 (function()  {
 	let tmpl = document.createElement('template');
 	tmpl.innerHTML = `
-	<style>
-	.text-field {
-		-moz-box-sizing: border-box;
-		border: 1px solid #EEEEEE;
-		font-family: "Source Sans Pro",Arial,sans-serif;
-		font-size: 0.73684em;
-		font-weight: 600;
-		height: 37px;
-		margin: 0;
-		padding: 5px;
-		width: 100%;
-		
-	}
-	.tfbutton2{
-		icon: "https://cdn3.vectorstock.com/i/1000x1000/62/77/lupe-icon-search-design-graphic-vector-10066277.jpg"
-	}
-	.autocomplete-suggestion {
-		overflow: hidden;
-		padding: 2px 5px;
-		white-space: nowrap;
-	}
-	.autocomplete-suggestions strong {
-		color: #3399FF;
-		font-weight: normal;
-	}
-	.autocomplete-selected{
-	  background:#F0F0F0;
-	}
-	</style>
-	<div>
-	<input type="submit" value=">" class="tfbutton2"><input type="text" id="query" class="text-field valid" autocomplete="on" placeholder="">
-	</div>
 	`;
 
 class ISearch extends HTMLElement {
@@ -67,10 +35,13 @@ class ISearch extends HTMLElement {
 			
 			
 			if (this._alive){
+				console.log('5');
 				return;
 			} else {
 			
+				console.log('10');
 				currentDiv = "DIV_" + this.$().attr('id');
+				console.log('20');
 				var currentSf = "SF_" + this.$().attr('id');
 				
 				// Create Search Field control and load data
