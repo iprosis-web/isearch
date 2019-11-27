1497a569966ac58872a4a73f3f4af0768353acd2
iprosis-web <evgeny.grin@iprosis.com>
2019-11-27 23:37:26 +0200

    explisis jquery

--- webcomponent.js
+++ webcomponent.js
@@ -2,19 +2,74 @@
 	let tmpl = document.createElement('template');
 	tmpl.innerHTML = `
 
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<style></style>
+		* {
+		box-sizing: border-box;
+		}
 
-	<script onload="console.log(ONE MOREEEEEE)" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-	<script onload="console.log('Loadedddddddd')" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-	
-	
-	
-		
+		body {
+		font: 16px Arial;  
+		}
 
-		<div class="ui-widget" id="container">
-  			<labelfor="tags">Tags: </label>
-  			<input id="tags" autocomplete="on">
+		/*the container must be positioned relative:*/
+		.autocomplete {
+		position: relative;
+		display: inline-block;
+		}
+
+		input {
+		border: 1px solid transparent;
+		background-color: #f1f1f1;
+		padding: 10px;
+		font-size: 16px;
+		}
+
+		input[type=text] {
+		background-color: #f1f1f1;
+		width: 100%;
+		}
+
+		input[type=submit] {
+		background-color: DodgerBlue;
+		color: #fff;
+		cursor: pointer;
+		}
+
+		.autocomplete-items {
+		position: absolute;
+		border: 1px solid #d4d4d4;
+		border-bottom: none;
+		border-top: none;
+		z-index: 99;
+		/*position the autocomplete items to be the same width as the container:*/
+		top: 100%;
+		left: 0;
+		right: 0;
+		}
+
+		.autocomplete-items div {
+		padding: 10px;
+		cursor: pointer;
+		background-color: #fff; 
+		border-bottom: 1px solid #d4d4d4; 
+		}
+
+		/*when hovering an item:*/
+		.autocomplete-items div:hover {
+		background-color: #e9e9e9; 
+		}
+
+		/*when navigating through the items using the arrow keys:*/
+		.autocomplete-active {
+		background-color: DodgerBlue !important; 
+		color: #ffffff; 
+
+		</style>
+
+		<div class="autocomplete id="container" style="width:300px;">
+			<input id="myInput" type="text" name="myCountry" placeholder="Country">
 		</div>
+
 
 	`;
 
@@ -26,35 +81,112 @@
 			shadow.appendChild(tmpl.content.cloneNode(true));
 
 			let containerDiv = shadow.querySelector('#container');
-			let btn1 = document.createElement('BUTTON');
-			btn1.id = 'btn1';
-			containerDiv.append(btn1);
 
-			let btn2 = document.createElement('BUTTON');
-			btn2.id = 'btn2';
-			containerDiv.append(btn2);
+			function autocomplete(inp, arr) {
+				var currentFocus;
+				inp.addEventListener('input', function(e) {
+					var a,
+						b,
+						i,
+						val = this.value;
+					closeAllLists();
+					if (!val) {
+						return false;
+					}
+					currentFocus = -1;
+					a = document.createElement('DIV');
+					a.setAttribute('id', this.id + 'autocomplete-list');
+					a.setAttribute('class', 'autocomplete-items');
+					this.parentNode.appendChild(a);
+					for (i = 0; i < arr.length; i++) {
+						if (
+							arr[i].substr(0, val.length).toUpperCase() ==
+							val.toUpperCase()
+						) {
+							b = document.createElement('DIV');
+							b.innerHTML =
+								'<strong>' +
+								arr[i].substr(0, val.length) +
+								'</strong>';
+							b.innerHTML += arr[i].substr(val.length);
+							b.innerHTML +=
+								"<input type='hidden' value='" + arr[i] + "'>";
 
-			btn1.onclick = function() {
-				console.log('Button1 !!!');
-				$('#btn2', shadow).html('Button2');
-				console.log($('#tags', shadow));
-			};
+							b.addEventListener('click', function(e) {
+								inp.value = this.getElementsByTagName(
+									'input'
+								)[0].value;
+								closeAllLists();
+							});
+							a.appendChild(b);
+						}
+					}
+				});
 
-			let availableTutorials = ['ActionScript', 'Bootstrap', 'C', 'C++'];
+				inp.addEventListener('keydown', function(e) {
+					var x = document.getElementById(
+						this.id + 'autocomplete-list'
+					);
+					if (x) x = x.getElementsByTagName('div');
+					if (e.keyCode == 40) {
+						currentFocus++;
+						addActive(x);
+					} else if (e.keyCode == 38) {
+						currentFocus--;
 
-			// $(window).load(function() {
-			// 	console.log('loaded!!!');
-			// });
+						addActive(x);
+					} else if (e.keyCode == 13) {
+						e.preventDefault();
+						if (currentFocus > -1) {
+							if (x) x[currentFocus].click();
+						}
+					}
+				});
+				function addActive(x) {
+					if (!x) return false;
 
-			// $('#tags', shadow).autocomplete({
-			// 	source: availableTutorials
-			// });
+					removeActive(x);
+					if (currentFocus >= x.length) currentFocus = 0;
+					if (currentFocus < 0) currentFocus = x.length - 1;
+					x[currentFocus].classList.add('autocomplete-active');
+				}
+				function removeActive(x) {
+					for (var i = 0; i < x.length; i++) {
+						x[i].classList.remove('autocomplete-active');
+					}
+				}
+				function closeAllLists(elmnt) {
+					var x = document.getElementsByClassName(
+						'autocomplete-items'
+					);
+					for (var i = 0; i < x.length; i++) {
+						if (elmnt != x[i] && elmnt != inp) {
+							x[i].parentNode.removeChild(x[i]);
+						}
+					}
+				}
+				document.addEventListener('click', function(e) {
+					closeAllLists(e.target);
+				});
+			}
 
-			// if (this._alive) {
-			// 	return;
-			// } else {
-			// 	this._alive = true;
-			// }
+			var countries = [
+				'Afghanistan',
+				'Albania',
+				'Algeria',
+				'Andorra',
+				'Angola',
+				'Anguilla',
+				'Antigua'
+			];
+
+			autocomplete(shadow.querySelector('#myInput'), countries);
+
+			if (this._alive) {
+				return;
+			} else {
+				this._alive = true;
+			}
 		}
 
 		// 	afterUpdate() {
