888507f9e433eca6e734d8a9c903b49815e6b105
iprosis-web <evgeny.grin@iprosis.com>
2019-11-30 23:00:23 +0200

    1

--- webcomponent.js
+++ webcomponent.js
@@ -77,7 +77,7 @@
 		selectedValue;
 		constructor() {
 			super();
-			this.selectedValue = 'test test test';
+
 			let shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(tmpl.content.cloneNode(true));
 
@@ -94,23 +94,24 @@
 						return false;
 					}
 					currentFocus = -1;
-					a = document.createElement('DIV');
-					a.setAttribute('id', this.id + 'autocomplete-list');
-					a.setAttribute('class', 'autocomplete-items');
+					let a = document.createElement('div');
+					a.id = this.id + 'autocomplete-list';
+					a.classList.add('autocomplete-items');
 					this.parentNode.appendChild(a);
 					for (i = 0; i < arr.length; i++) {
 						if (
 							arr[i].substr(0, val.length).toUpperCase() ==
 							val.toUpperCase()
 						) {
-							b = document.createElement('DIV');
+							b = document.createElement('div');
 							b.innerHTML =
 								'<strong>' +
 								arr[i].substr(0, val.length) +
-								'</strong>';
-							b.innerHTML += arr[i].substr(val.length);
-							b.innerHTML +=
-								"<input type='hidden' value='" + arr[i] + "'>";
+								'</strong>' +
+								arr[i].substr(val.length) +
+								"<input type='hidden' value='" +
+								arr[i] +
+								"'>";
 
 							b.addEventListener('click', function(e) {
 								inp.value = this.getElementsByTagName(
@@ -194,198 +195,6 @@
 		// get value() {
 		// 	return this.countries;
 		// }
-
-		// 	afterUpdate() {
-		// 			that.oSearchField.setEnabled(isEnabled);
-		// 			that.oSearchField.setPlaceholder(placeHolder);
-		// 			that.oSearchField.setEnableSuggestions(isSuggestions);
-		// 			that.oSearchField.setMaxLength(maxLength);
-		// 			that.oSearchField.setShowSearchButton(isSearchButton);
-
-		// 			if (dataResultSet){
-		// 				if (reload){
-		// 					return;
-		// 				} else {
-		// 					this.insertData();
-		// 					reload = true;
-		// 				}
-		// 			}
-
-		// 	};
-
-		// 	getFilters = function(value){
-		// 			var filters = [];
-		// 			oFilterText = new sap.ui.model.Filter("text", function(sText) {
-		// 				return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-		// 			});
-		// 			oFilterDesc =  new sap.ui.model.Filter("key", function(sDes) {
-		// 				 	return (sDes || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-		// 				 });
-
-		// 			if (displayKey === true)
-		// 				{
-		// 					filters = [new sap.ui.model.Filter([oFilterText,oFilterDesc], false)];
-		// 				}else{
-		// 					filters = [	new sap.ui.model.Filter([oFilterText], false)];
-		// 				}
-
-		// 			return filters
-		// 	};
-
-		// 	getTemplate = function(){
-		// 			if (displayKey === true)
-		// 				{
-		// 					 oTemplate = new sap.m.SuggestionItem({
-		// 						text: "{text}",
-		// 						key: "{key}",
-		// 						description:"{key}"
-		// 					 });
-		// 				}else{
-		// 					oTemplate = new sap.m.SuggestionItem({
-		// 					text: "{text}",
-		// 					key: "{key}"
-		// 				});
-		// 			}
-		// 			return oTemplate;
-		// 	};
-
-		// 	getData = function(result){
-		// 			var data = [];
-		// 			var x = result;
-		// 			var obj = {};
-		// 			result.forEach(function(element) {
-		// 				obj = {};
-		// 				obj.text = element.text;
-		// 				obj.key = element.key;
-		// 				data.push(obj);
-		// 			});
-
-		// 			var oModel = new sap.ui.model.json.JSONModel(data);
-		// 			var oTemplate = this.getTemplate();
-		// 			that.oSearchField.setModel(oModel);
-		// 			that.oSearchField.bindAggregation("suggestionItems", "/", oTemplate);
-
-		// 	};
-
-		// 	insertData = function() {
-
-		// 			if (selectedDimension)
-		// 				{
-		// 				var dim = selectedDimension.toUpperCase();
-		// 					that.callZTLFunction("getMembers", that.getData, dim);
-		// 				}
-
-		// //			data = dataResultSet.dimensions[0].members;
-		// //			oModel.setSizeLimit(maxItems);
-		// 	};
-
-		// 	DataResultSet = function(value) {
-		// 			if(value===undefined) {
-		// 				return dataResultSet;
-		// 			} else {
-		// 				//Clear Auto
-		// 				if (reload) {
-		// 					that.oSearchField.removeAllSuggestionItems();
-		// 					that.oSearchField.setValue("");
-		// 					reload = false;
-		// 				}
-		// 				dataResultSet = value;
-		// 				return this;
-		// 			};
-		// 	};
-
-		// 	DisplayKey = function(value) {
-		// 			if(value===undefined) {
-		// 				Reload = false;
-		// 				return displayKey;
-		// 			} else {
-		// 				Reload = false;
-		// 				displayKey = value;
-		// 				return this;
-		// 			};
-		// 	};
-
-		// 	IsEnabled = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return isEnabled;
-		// 		} else {
-		// 			Reload = false;
-		// 			isEnabled = value;
-		// 			return this;
-		// 		};
-		// 	};
-
-		// 	PlaceHolder = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return placeHolder;
-		// 		} else {
-		// 			Reload = false;
-		// 			placeHolder = value;
-		// 			return this;
-		// 		};
-		// 	};
-
-		// 	IsSearchButton = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return isSearchButton;
-		// 		} else {
-		// 			Reload = false;
-		// 			isSearchButton = value;
-		// 			return this;
-		// 		};
-		// 	};
-		// 	MaxLength = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return maxLength;
-		// 		} else {
-		// 			Reload = false;
-		// 			maxLength = value;
-		// 			return this;
-		// 		};
-		// 	};
-		// 	SelectedDimension = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return selectedDimension;
-		// 		} else {
-		// 			Reload = false;
-		// 			selectedDimension = value;
-		// 			return this;
-		// 		};
-		// 	};
-
-		// 	IsSuggestions = function(value) {
-		// 		if(value===undefined) {
-		// 			Reload = false;
-		// 			return isSuggestions;
-		// 		} else {
-		// 			Reload = false;
-		// 			isSuggestions = value;
-		// 			return this;
-		// 		};
-		// 	};
-
-		// 	SelectedValue = function(value) {
-		// 		if(value===undefined) {
-		// 			return selectedValue;
-		// 		} else {
-		// 			selectedValue = value;
-		// 			return this;
-		// 		};
-		// 	};
-
-		// 	SelectedText = function(value) {
-		// 		if(value===undefined) {
-		// 			return selectedText;
-		// 		} else {
-		// 			selectedText = value;
-		// 			return this;
-		// 		};
-		// 	};
 	}
 
 	/* Define web component - input: tag and class */
